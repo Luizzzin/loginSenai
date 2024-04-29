910df867f0dedad24518da55abe036e381a9c445
Luizzzin <luizhenribarbodias@gmail.com>
2024-04-29 14:41:47 -0300

    Initial commit

---
+++ pages/entrada.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import * as Animatable from 'react-native-animatable';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+  StatusBar
+} from 'react-native';
+
+export default function Acesso() {
+  return (
+    <KeyboardAvoidingView style={{ flex: 1 }}>
+      <StatusBar backgroundColor={"#880000"}/>
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <Animatable.View animation="fadeInUp" delay={200} style={styles.container}>
+          <Animatable.View animation="fadeInLeft" delay={610} style={styles.containerHeader}>
+            <Text style={styles.message}>Bem-vindo(a)</Text>
+          </Animatable.View>
+          <View style={styles.containerForm}>
+            <Text style={styles.title}>
+              E-mail
+            </Text>
+            <TextInput
+              placeholder='Digite um email...'
+              style={styles.input}
+            />
+            <TextInput
+              placeholder='Sua senha'
+              style={styles.input}
+            />
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>
+                Acessar
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonRegister}>
+              <Text style={styles.registerText}>
+                Não possui uma conta? Cadastre-se
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </Animatable.View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#880000'
+  },
+  containerHeader: {
+    marginTop: '14%',
+    marginBottom: '8%',
+    paddingStart: '5%',
+  },
+  message: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: "#FFF"
+  },
+  containerForm: {
+    backgroundColor: "#FFF",
+    flex: 1,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingStart: "5%",
+    paddingEnd: "5%"
+  },
+  title: {
+    fontSize: 22,
+    marginTop: 28,
+  },
+  input: {
+    borderBottomWidth: 1,
+    height: 40,
+    marginBottom: 15,
+    fontSize: 16
+  },
+  button: {
+    backgroundColor: '#880000',
+    width: '100%',
+    borderRadius: 4,
+    paddingVertical: 8,
+    marginTop: 22,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 22,
+    fontWeight: 'bold'
+  },
+  buttonRegister: {
+    marginTop: 14,
+    alignSelf: 'center'
+  },
+  registerText: {
+    color: '#a1a1a1'
+  }
+})
---
+++ pages/index.js
@@ -0,0 +1,92 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import * as Animatable from 'react-native-animatable';
+
+export default function Bem_Vindo() {
+
+    const navigation = useNavigation();
+    useFonts({ 'Oswald': require('../assets/fonts/Oswald-VariableFont_wght.ttf') });
+
+    return (
+        <Animatable.View delay={100} animation='fadeInUp' style={styles.container}>
+            <StatusBar backgroundColor={"#FFF"}/>
+            <View style={styles.containerLogo}>
+                <Animatable.Image
+                    delay={610}
+                    animation="fadeInDown"
+                    source={require("../assets/logo_Senai.png")}
+                    style={styles.logo}
+                    resizeMode="contain"
+                />
+            </View>
+            <View style={styles.containerForm}>
+                <Text style={styles.title}>
+                    Monitore e organize seus recursos didáticos de qualquer lugar!
+                </Text>
+                <Text style={styles.text}>
+                    Faça o login para começar
+                </Text>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate('entrada')}
+                    style={styles.button}>
+                    <Text style={styles.buttonText}>
+                        Acessar
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </Animatable.View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF'
+    },
+    containerLogo: {
+        flex: 1,
+        backgroundColor: '#FFF',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    logo: {
+        width: "70%"
+
+    },
+    containerForm: {
+        flex: 1,
+        backgroundColor: '#880000',
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        paddingStart: '5%',
+        paddingEnd: '5%'
+    },
+    title: {
+        fontSize: 28,
+        marginTop: 20,
+        marginBottom: 12,
+        color: "#FFF",
+        fontFamily: 'Oswald',
+    },
+    text: {
+        color: '#a1a1a1',
+        fontSize : 18,
+    },
+    button: {
+        position: 'absolute',
+        backgroundColor: '#FFF',
+        borderRadius: 50,
+        paddingVertical: 8,
+        width: '65%',
+        alignSelf: 'center',
+        bottom: '35%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        fontSize: 22,
+        color: '#880000',
+        fontWeight: 'bold'
+    }
+})
